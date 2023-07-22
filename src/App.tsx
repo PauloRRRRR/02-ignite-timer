9f8a9fd7dfe4246937c596503487454aabcf6f4d
Paulo Ricardo <notadead5@gmail.com>
2023-07-22 20:07:52 -0300

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import {ThemeProvider} from 'styled-components'
+import { Button } from './components/Button'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello world</h1>
+    <ThemeProvider theme={defaultTheme} >
+      <Button variant='primary' />
+      <Button variant='secondary' />
+      <Button variant='danger' />
+      <Button variant='success' />
+
+      <GlobalStyle/>
+
+    </ThemeProvider>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
-import { Button } from './components/Button.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Button variant='primary' />
-    <Button variant='secondary' />
-    <Button variant='danger' />
-    <Button variant='success' />
-    <Button/>
+    
   </React.StrictMode>,
 )
